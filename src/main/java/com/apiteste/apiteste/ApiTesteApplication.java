5ca62b2a10e87f8a97c468adbbf1a1f6f574d735
cristovaobplima <89477878+cristovaobplima@users.noreply.github.com>
2024-01-10 20:59:47 -0300

    upload api class

--- src/main/java/com/apiteste/apiteste/ApiTesteApplication.java
+++ src/main/java/com/apiteste/apiteste/ApiTesteApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class ApiTesteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiTesteApplication.class, args);
 	}
+	@GetMapping("/hello")
+	public String Hello() {
 
+		return	"Hello World!";
+
+	}
 }
